b21b5310e9b42806277d47dc58bc4aace62ce440
Eugene Ustimenko <evgenyustimenko@gmail.com>
2016-08-15 14:13:45 +0600

    Expand resource properties

--- web/src/main/resources/static/js/controller/products.js
+++ web/src/main/resources/static/js/controller/products.js
@@ -1,4 +1,5 @@
 app.controller("productsController", function ($scope, $http) {
+
     $http.get('http://localhost:8081/api/product').success(function (data) {
         $scope.products = data;
     });
